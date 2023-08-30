3d575735539c903f2aae569132e10dc8e27cc235
Christian Llovera <christian.llovera@telsone.it>
2023-08-30 15:58:53 +0200

    added spring jwt authentication (controller and service)

---
+++ src/main/java/poicity/controller/AuthController.java
@@ -0,0 +1,24 @@
+package poicity.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	@PostMapping("login")
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping("register")
+	public String register() {
+		return "register";
+	}
+	
+}
