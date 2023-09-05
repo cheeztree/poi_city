860b260a03048c3ec994e7631e26c1dcb11843c1
Christian Llovera <christian.llovera@telsone.it>
2023-09-05 12:57:45 +0200

    added rout, classes and utils for reset password

--- src/main/java/poicity/controller/AuthController.java
+++ src/main/java/poicity/controller/AuthController.java
@@ -16,10 +16,13 @@ import lombok.RequiredArgsConstructor;
 import poicity.dto.AuthResponse;
 import poicity.dto.ErrorDTO;
 import poicity.dto.LoginDTO;
+import poicity.dto.ResetPasswordDTO;
 import poicity.dto.UserDTO;
 import poicity.entity.User;
 import poicity.repository.UserRepository;
 import poicity.service.AuthService;
+import poicity.utils.JavaMail;
+import poicity.utils.PasswordGenerator;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,36 +32,59 @@ public class AuthController {
 	@Autowired
 	private UserRepository userRepo;
 	private final AuthService authService;
-    private final PasswordEncoder passwordEncoder;
-		
-	@PostMapping("login")
+	private final PasswordEncoder passwordEncoder;
+
+	@PostMapping("/login")
 	public ResponseEntity<Object> login(@RequestBody LoginDTO request) {
-		if(userRepo.existsByEmail(request.getEmail())){	
+		if (userRepo.existsByEmail(request.getEmail())) {
 			User user = userRepo.findByEmail(request.getEmail());
-	        if(passwordEncoder.matches(String.valueOf(request.getPassword()), user.getPassword())) {
+			if (passwordEncoder.matches(String.valueOf(request.getPassword()), user.getPassword())) {
 				return ResponseEntity.ok(authService.login(request));
-	        } else {				
+			} else {
 				return new ResponseEntity<Object>(new ErrorDTO(new Date(), "Invalid password."), HttpStatus.NOT_FOUND);
 			}
 		} else {
-			return new ResponseEntity<Object>(new ErrorDTO(new Date(), request.getEmail() +  "' doesn''t exists."), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<Object>(new ErrorDTO(new Date(), request.getEmail() + "' doesn''t exists."),
+					HttpStatus.NOT_FOUND);
 		}
 	}
-	
-	@PostMapping("register")
+
+	@PostMapping("/register")
 	public ResponseEntity<Object> register(@RequestBody UserDTO request) {
 		System.out.println(request);
-		if(userRepo.existsByEmail(request.getEmail())){
-			return new ResponseEntity<Object>(new ErrorDTO(new Date(), "User with email '" + request.getEmail() +  "' already exists."), HttpStatus.CONFLICT);
+		if (userRepo.existsByEmail(request.getEmail())) {
+			return new ResponseEntity<Object>(
+					new ErrorDTO(new Date(), "User with email '" + request.getEmail() + "' already exists."),
+					HttpStatus.CONFLICT);
 		} else {
-			System.out.println("ASDASDASDASDASD");
 			return ResponseEntity.ok(authService.register(request));
 		}
 	}
-	
-	@GetMapping("google")
-	public ResponseEntity<String> hello(){
-		return new ResponseEntity<>("ASDASDASDASDASDASD", HttpStatus.CREATED);
+
+	@PostMapping("/resetPassword")
+	public ResponseEntity<Object> register(@RequestBody ResetPasswordDTO resetPassDTO) {
+		System.out.println(resetPassDTO.getEmail());
+		String email = resetPassDTO.getEmail();
+		if (userRepo.existsByEmail(email)) {
+			
+			User user = userRepo.findByEmail(email);
+			String newPassword = PasswordGenerator.generate();
+			user.setPassword(passwordEncoder.encode(newPassword));
+
+			userRepo.save(user);
+			
+			JavaMail.mandaEmailXresetPass(user.getEmail(), newPassword, user.getName());
+			
+		} else {
+			return new ResponseEntity<Object>(new ErrorDTO("User with this email doesn't exists."), HttpStatus.NOT_FOUND);
+		}
+
+		return null;
 	}
-	
+
+	// @GetMapping("google")
+	// public ResponseEntity<String> hello(){
+	// return new ResponseEntity<>("ASDASDASDASDASDASD", HttpStatus.CREATED);
+	// }
+
 }
