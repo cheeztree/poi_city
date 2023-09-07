38c30bf6dbb101b9dde5905130a0b060602b05df
Christian Llovera <christian.llovera@telsone.it>
2023-09-07 15:03:40 +0200

    handled exceptions and errors for upload and download user images

--- src/main/java/poicity/controller/UserController.java
+++ src/main/java/poicity/controller/UserController.java
@@ -1,10 +1,16 @@
 package poicity.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import lombok.AllArgsConstructor;
 import poicity.dto.ErrorDTO;
 import poicity.dto.UserDTO;
@@ -30,23 +35,20 @@ import poicity.utils.FilesUtils;
 @AllArgsConstructor
 public class UserController {
 
-	//	private ModelMapper mapper;
-
 	private MyMapper mapper;
 	private UserRepository userRepo;
-//	private UserService userService;
 	private RoleRepository roleRepository;
 
 	@PostMapping("create")
 	public ResponseEntity<UserDTO> add(@RequestBody UserDTO userDTO) {
-		if(userDTO.getAvatar().equals("") || userDTO.getAvatar() == null || userDTO.getAvatar().equals("string")) {
+		if (userDTO.getAvatar().equals("") || userDTO.getAvatar() == null || userDTO.getAvatar().equals("string")) {
 			userDTO.setAvatar(FilesUtils.immagazzinaAvatarDefault2());
 		}
-		
+
 		userRepo.save(mapper.map(userDTO, User.class));
 
 		return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
-		
+
 	}
 
 	@GetMapping("getById/{id}")
@@ -108,35 +110,120 @@ public class UserController {
 		}
 	}
 
-	
 	@PostMapping("/uploadImgUser")
-	public ResponseEntity<?> provaUploadImg(@RequestParam("image")MultipartFile file, @RequestParam(value="email") String email) {
-		if(!userRepo.existsByEmail(email)) {
-			return new ResponseEntity<>(new ErrorDTO("User with email '" + email + "' already exists."), HttpStatus.UNAUTHORIZED);
+	public ResponseEntity<?> uploadImgUser(@RequestParam("image") MultipartFile file,
+			@RequestParam(value = "email") String email) {
+		if (!userRepo.existsByEmail(email)) {
+			return new ResponseEntity<>(new ErrorDTO("User with email '" + email + "' already exists."),
+					HttpStatus.UNAUTHORIZED);
 		}
-		
-		if(byteToMB(file.getSize())>5) {
-			return new ResponseEntity<>(new ErrorDTO("Files sizes limit is 5MB"), HttpStatus.UNAUTHORIZED);	
+
+		if (byteToMB(file.getSize()) > 5) {
+			return new ResponseEntity<>(new ErrorDTO("Files sizes limit is 5MB"), HttpStatus.UNAUTHORIZED);
 		}
-		
+
 		String pathImg = FilesUtils.immagazzinaImg(file);
-		
+
 		User user = userRepo.findByEmail(email);
 		user.setAvatar(pathImg);
-		
+
 		userRepo.save(user);
-		
+
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
-	
+
 	private double byteToMB(long bytes) {
-		double MB = (double)bytes / 1024 / 1024;
+		double MB = (double) bytes / 1024 / 1024;
 		return MB;
 	}
-	
-	//    @GetMapping("affila")
-	//    public String affila() {
-	//    	return "AFFILA";
-	//    }
+
+	@GetMapping(value = "/avatarByPath/{path}", produces = "image/*")
+	public ResponseEntity<byte[]> getImage(@PathVariable("path") String path) throws IOException {
+
+		InputStream is = new FileInputStream(path);
+		byte[] bytes = StreamUtils.copyToByteArray(is);
+
+		return new ResponseEntity<>(bytes, HttpStatus.OK);
+
+	}
+
+	@GetMapping(value = "/avatar/{UserId}", produces = "image/*")
+	public ResponseEntity<?> getImageByUserId(@PathVariable("UserId") Long UserId) throws IOException {
+
+		User user;
+
+		try {
+			user = userRepo.findById(UserId).get();
+		} catch (Exception e) {
+//    		e.printStackTrace();
+			
+			HttpHeaders responseHeaders = new HttpHeaders();
+			responseHeaders.setContentType(new MediaType("application", "json"));
+
+			ResponseEntity<?> res = new ResponseEntity<>(new ErrorDTO("User not found."), responseHeaders, HttpStatus.NOT_FOUND);
+			return res;
+			
+
+		}
+
+		try {
+			InputStream is = new FileInputStream(user.getAvatar());
+			byte[] bytes = StreamUtils.copyToByteArray(is);
+			is.close();
+
+//            HttpHeaders responseHeaders = new HttpHeaders();
+//            responseHeaders.setContentType(new MediaType("image", "*"));
+
+			return new ResponseEntity<>(bytes
+//    				, responseHeaders
+					, HttpStatus.OK);
+		} catch (Exception e) {
+//    		e.printStackTrace();
+//			return new ResponseEntity<>("Image not found", HttpStatus.NOT_FOUND);
+
+			HttpHeaders responseHeaders = new HttpHeaders();
+			responseHeaders.setContentType(new MediaType("application", "json"));
+
+			ResponseEntity<?> res = new ResponseEntity<>(new ErrorDTO("Image not found."), responseHeaders, HttpStatus.NOT_FOUND);
+			return res;
+		}
+
+	}
+
+//	@GetMapping("/downloadImgUser")
+//	public ResponseEntity<Resource> downloadImgUser(
+////			Authentication authentication
+//			) throws IOException {
+////		User user = userRepo.findByEmail(authentication.getName());
+//		User user = userRepo.findByEmail("christian.llovera@telsone.it");
+//		
+//	    Path path = Paths.get(user.getAvatar());
+//	    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+//
+////	    return ResponseEntity.ok()
+////	            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+////	            .body(resource);
+//	    
+//		return new ResponseEntity<>(resource, HttpStatus.OK);
+//
+//
+//		
+//	}
+
+//	@GetMapping("/download/{fileImg}")
+//	public ResponseEntity<Resource> downloadImgUser(@PathVariable("fileImg") String fileImg) throws IOException{
+//		User user = userRepo.findByEmail("christian.llovera@telsone.it");
+//		
+//	    Path path = Paths.get(user.getAvatar());
+//	    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get("C:\\Users\\Telsone\\eclipse-workspace\\poi_city_be\\img\\"+fileImg)));
+//	    
+//		return new ResponseEntity<>(resource, HttpStatus.OK);
+//
+//	}
+
+	// @GetMapping("affila")
+	// public String affila() {
+	// return "AFFILA";
+	// }
 
 }
