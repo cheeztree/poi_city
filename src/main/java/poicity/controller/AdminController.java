15e6a1d9bb665c83bbaa1c033865cd29fa16acd3
Christian Llovera <c.llovera@studenti.poliba.it>
2023-09-19 16:44:16 +0200

    added token check for /admin/panel and correctly setted all route for admin

--- src/main/java/poicity/controller/AdminController.java
+++ src/main/java/poicity/controller/AdminController.java
@@ -1,16 +1,34 @@
 package poicity.controller;
 
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingRequestCookieException;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -31,7 +49,7 @@ import poicity.service.UserService;
 @RequestMapping("admin")
 @RequiredArgsConstructor
 public class AdminController {
-	
+
 	@Autowired
 	private UserRepository userRepo;
 	@Autowired
@@ -43,47 +61,123 @@ public class AdminController {
 	private final CustomUserDetailsService customUserDetailsService;
 	private final JwtService jwtService;
 	private final RoleRepository roleRepo;
-    @GetMapping
-    public String index(Model model) {
-    	model.addAttribute("chiave", "valoreeeeeeeee");
-        return "admin_login";
-    }
-    
-    @PostMapping
-    public String login(@ModelAttribute(name = "loginForm") LoginDTO loginDTO, Model model, HttpServletResponse res) {
-//    	System.out.println(loginDTO);
-    	
+
+	@GetMapping
+	public Object index(
+//    		@CookieValue(value = "token", required = false) String token
+	) {
+
+		return "admin_login";
+//    	if(token == null) {
+//            return "admin_login";
+//    	} else {
+//    		
+//    		System.out.println(authentication == null);
+//    		
+//    		HttpHeaders responseHeaders = new HttpHeaders();
+//    	    responseHeaders.set("authorization", token);
+//    	    System.out.println("TOKEN " + token);
+//    	    return new ResponseEntity<String>("admin_panel", responseHeaders, HttpStatus.OK);
+//    		
+//    		
+////            return "admin_panel";
+//    	}
+
+	}
+
+	@PostMapping(value = "/login")
+	public String login(@ModelAttribute(name = "loginForm") LoginDTO loginDTO, HttpServletResponse res) {
+
 		if (userService.existsByEmail(loginDTO.getEmail())) {
 			User user = userService.findByEmail(loginDTO.getEmail());
 			Role roleAdmin = roleRepo.findByName("ADMIN");
 //			System.out.println(user);
 //			System.out.println(user.getRoles().contains(role));
 
-			if(!user.getRoles().contains(roleAdmin)){
-				return "redirect:admin?notAdmin";
+			if (!user.getRoles().contains(roleAdmin)) {
+				return "redirect:/admin?notAdmin";
 			}
-			
+
 			if (passwordEncoder.matches(String.valueOf(loginDTO.getPassword()), user.getPassword())) {
 
 				String token = authService.login(loginDTO).getToken();
-				
+
 				Cookie cookie = new Cookie("token", token);
 				cookie.setMaxAge(Integer.MAX_VALUE);
 				res.addCookie(cookie);
-				
+
 //				return "admin_panel";
 			} else {
-				System.out.println("PASSWORD NON MATCHA");
-	            
-				return "redirect:admin?wrongPassword";
+//				System.out.println("PASSWORD NON MATCHA");
+
+				return "redirect:/admin?wrongPassword";
 			}
 		} else {
-			System.out.println("NON esiste mail");
-            
-			return "redirect:admin?wrongMail";
+//			System.out.println("NON esiste mail");
+
+			return "redirect:/admin?wrongMail";
 
 		}
-	
-        return "admin_panel";
-    }
+
+//		HttpHeaders responseHeaders = new HttpHeaders();
+//	    responseHeaders.set("authorization", tokenCookie);
+//	    
+//	    return new ResponseEntity<String>("admin_panel", responseHeaders, HttpStatus.OK);
+//        return "admin_panel";
+
+		// TUTTO OKAY
+		return "redirect:/admin/panel";
+
+	}
+
+	@GetMapping("/panel")
+	public String panel(@CookieValue(value = "token", required = false) String token) {
+		if (token == null) {
+			System.out.println("TOKEN NULLO");
+		} else {
+//			System.out.println(token);
+
+			RestTemplate restTemplate = new RestTemplate();
+
+			HttpHeaders headers = new HttpHeaders();
+//			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + token);
+			
+			Map<String, Object> map = new HashMap<>();
+
+			HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+			System.out.println(entity);
+			
+			ResponseEntity<String> respEntity = restTemplate.exchange("http://localhost:8081/auth/checkToken",
+					HttpMethod.POST, entity, String.class);
+
+			System.out.println(respEntity);
+			System.out.println(respEntity.getBody());
+			
+//			String response = restTemplate.getForObject("http://localhost:8081/lang/getOnlyActive", String.class);
+//			System.out.println(response);
+
+			// String body = responseEntity.getBody();
+//			HttpHeaders headers = responseEntity.getHeaders();
+//			
+//			System.out.println(body);
+//			System.out.println(headers);
+
+//			HttpEntity<Foo> request = new HttpEntity<>();
+//			Foo foo = restTemplate.postForObject(fooResourceUrl, request, Foo.class);
+//			Assertions.assertNotNull(foo);
+//			Assertions.assertEquals(foo.getName(), "bar");
+
+		}
+		return "admin_panel";
+	}
+
+	@GetMapping("/getAllUsers")
+	public String getAllUsers(Model model) {
+		List<User> listUser = userRepo.findAll();
+		model.addAttribute("listUser", listUser);
+
+		return "listaUsers";
+	}
 }
