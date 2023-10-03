f55a76768d50249f46832b4d881c8fe1868f317c
Christian Llovera <christian.llovera@telsone.it>
2023-10-03 12:10:31 +0200

    remove https

--- src/main/java/poicity/Main.java
+++ src/main/java/poicity/Main.java
@@ -18,17 +18,17 @@ public class Main extends SpringBootServletInitializer  {
 //	}
 	
 	public static void main(String[] args) {
-//		Properties props = getInit();
+		Properties props = getInit();
 //		new SpringApplicationBuilder(Main.class).properties(props).run(args);
 
-		Properties props = new Properties();
-		props.put("server.port", "8081");
-		props.put("spring.datasourc.url", "jdbc:mysql://185.81.4.53:3306/baritst_poicity");
-		props.put("spring.datasource.username", "baritst_root");
-		props.put("spring.datasource.password", "G0K5#+5Irp.9");
-		props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-		props.put("logo", "https://i.ibb.co/c6GGm20/logo.png");
-		props.put("defaultAvatar", "https://i.ibb.co/xGZtcXS/dafault-avatar.png");
+//		Properties props = new Properties();
+//		props.put("server.port", "8081");
+//		props.put("spring.datasourc.url", "jdbc:mysql://185.81.4.53:3306/baritst_poicity");
+//		props.put("spring.datasource.username", "baritst_root");
+//		props.put("spring.datasource.password", "G0K5#+5Irp.9");
+//		props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//		props.put("logo", "https://i.ibb.co/c6GGm20/logo.png");
+//		props.put("defaultAvatar", "https://i.ibb.co/xGZtcXS/dafault-avatar.png");
 		
 		new SpringApplicationBuilder(Main.class).properties(props).run(args);
 				
